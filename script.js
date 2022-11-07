9793f94f19a09aa4a5743516aebf89fd2c80bc4b
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-11-07 20:49:02 +0100

    commit

--- script.js
+++ script.js
@@ -1,40 +1,58 @@
-let computerGuess
-const guesses = []
+let computerGuess;
+const guesses = [];
+let attempts = 0;
+let maxGuesses;
 
 function init() {
     computerGuess = Math.floor(Math.random() * 100 + 1)
     console.log(computerGuess)
     document.getElementById("gameArea").style.display = "none"
     document.getElementById("newGameButton").style.display = "none"
-}
+};
 
 function startGame() {
     document.getElementById("gameArea").style.display = "block"
     document.getElementById("welcomeScreen").style.display = "none"
-}
+};
 
 function easyMode() {
+    maxGuesses = 5
     startGame()
-}
+};
 
 function hardMode() {
+    maxGuesses = 10
     startGame()
-}
+};
 
 function compareGuess() {
     const userGuess = Number(document.getElementById("inputBox").value)
     guesses.push(" " + userGuess)
     document.getElementById("previousGuesses").innerHTML = guesses
+    attempts ++
+    document.getElementById("attempts").innerHTML = attempts
 
-    if (userGuess > computerGuess) {
-        document.getElementById("textOutput").innerHTML = "Your guess is too high"
-        document.getElementById("inputBox").value = ""
-
-    } else if (userGuess < computerGuess) {
-        document.getElementById("textOutput").innerHTML = "Your guess is too low"
-        document.getElementById("inputBox").value = ""
-
+    if (attempts < maxGuesses) {
+        if (userGuess > computerGuess) {
+            document.getElementById("textOutput").innerHTML = "Your guess is too high"
+            document.getElementById("inputBox").value = ""
+    
+        } else if (userGuess < computerGuess) {
+            document.getElementById("textOutput").innerHTML = "Your guess is too low"
+            document.getElementById("inputBox").value = ""
+    
+        } else {
+            document.getElementById("textOutput").innerHTML = `Correct! You got it at ${attempts} attempts`
+        }
     } else {
-        document.getElementById("textOutput").innerHTML = "Correct!"
+        if (userGuess > computerGuess) {
+            document.getElementById("textOutput").innerHTML = `YOU LOSE!, <br> The number is ${computerGuess}`
+    
+        } else if (userGuess < computerGuess) {
+            document.getElementById("textOutput").innerHTML = `YOU LOSE! <br> The number is ${computerGuess}`
+    
+        } else {
+            document.getElementById("textOutput").innerHTML = `Correct! You got it at ${attempts} attempts`
+        }
     }
-}
+};
